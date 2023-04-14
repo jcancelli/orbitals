e7ec8e313e80ef7bc4cefa6d7e762917ad087b71
jcancelli <jonathan.cancelli.46@gmail.com>
2023-04-14 15:19:09 +0200

    add math/trigonometry.hpp

---
+++ src/cpp/math/trigonometry.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <type_traits>
+
+#include "vector.hpp"
+
+namespace math {
+
+template <typename T>
+inline T radians(T deg) {
+  static_assert(std::is_arithmetic<T>::value, "Invalid parameter type");
+  return deg * static_cast<T>(0.01745329251994329576923690768489);
+}
+
+template <typename T>
+inline T degrees(T rad) {
+  static_assert(std::is_arithmetic<T>::value, "Invalid parameter type");
+  return rad * static_cast<T>(57.295779513082320876798154814105);
+}
+
+};  // namespace math
