91e000075dc51e84b6d063c9bdf6f5f61017739c
jcancelli <jonathan.cancelli.46@gmail.com>
2023-04-16 19:13:08 +0000

    add program instantiation and viewport size synchronization

--- src/frontend/routes/+layout.svelte
+++ src/frontend/routes/+layout.svelte
@@ -1,24 +1,30 @@
 <script lang="ts">
 	import "../app.css";
 	import Viewport from "$lib/components/Viewport.svelte";
-	import loadWasmModule from "$lib/wasm.mjs";
+	import loadWasmModule from "$lib/wasm/wasm.mjs";
 	import { onMount, onDestroy } from "svelte";
 	import { base } from "$app/paths";
+	import type OrbitalsWasmModule from "$lib/wasm/OrbitalsWasmModule";
+	import type { Orbitals } from "$lib/wasm/OrbitalsWasmModule";
 
 	let viewportWidth: number;
 	let viewportHeight: number;
 
+	let wasmModule: OrbitalsWasmModule;
+	let orbitals: Orbitals;
+
 	onMount(async () => {
 		window.addEventListener("resize", resizeViewport);
-		resizeViewport();
 
 		const moduleOverrides: Partial<EmscriptenModule> = {
 			locateFile: (path: string, prefix: string) => {
 				return base + "/" + path;
 			}
 		};
+		wasmModule = await loadWasmModule(moduleOverrides);
+		orbitals = new wasmModule.Orbitals();
 
-		const wasmModule: EmscriptenModule = await loadWasmModule(moduleOverrides);
+		resizeViewport();
 	});
 
 	onDestroy(() => {
@@ -28,6 +34,8 @@
 	function resizeViewport(): void {
 		viewportWidth = window.innerWidth;
 		viewportHeight = window.innerHeight;
+		orbitals.setViewportWidth(viewportWidth);
+		orbitals.setViewportHeight(viewportHeight);
 	}
 </script>
 
