bf2c6bd8782f5d9989a562f91dacd93bcbdb451d
jcancelli <jonathan.cancelli.46@gmail.com>
2023-04-16 17:05:23 +0000

    implement Shader uniforms

--- src/cpp/engine/Shader.cpp
+++ src/cpp/engine/Shader.cpp
@@ -21,6 +21,7 @@ static GLuint compileShader(char const* src, GLenum shaderType) {
               << " shader: " << errorMessage << std::endl;
     assert(false);
   }
+  return id;
 }
 
 namespace engine {
@@ -50,6 +51,70 @@ Shader::~Shader() {
   glCall(glDeleteProgram(m_Id));
 }
 
+void Shader::setUniform1f(std::string const& key, GLfloat v0) {
+  GLuint uniformLocation = getUniformLocation(key);
+  if (uniformLocation == -1) {
+    return;
+  }
+  glCall(glUniform1f(uniformLocation, v0));
+}
+
+void Shader::setUniform2f(std::string const& key, GLfloat v0, GLfloat v1) {
+  GLuint uniformLocation = getUniformLocation(key);
+  if (uniformLocation == -1) {
+    return;
+  }
+  glCall(glUniform2f(uniformLocation, v0, v1));
+}
+
+void Shader::setUniform2f(std::string const& key, math::vec2 const& v) {
+  GLuint uniformLocation = getUniformLocation(key);
+  if (uniformLocation == -1) {
+    return;
+  }
+  glCall(glUniform2f(uniformLocation, v[0], v[1]));
+}
+
+void Shader::setUniform3f(std::string const& key, GLfloat v0, GLfloat v1, GLfloat v2) {
+  GLuint uniformLocation = getUniformLocation(key);
+  if (uniformLocation == -1) {
+    return;
+  }
+  glCall(glUniform3f(uniformLocation, v0, v1, v2));
+}
+
+void Shader::setUniform3f(std::string const& key, math::vec3 const& v) {
+  GLuint uniformLocation = getUniformLocation(key);
+  if (uniformLocation == -1) {
+    return;
+  }
+  glCall(glUniform3f(uniformLocation, v[0], v[1], v[2]));
+}
+
+void Shader::setUniform4f(std::string const& key, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3) {
+  GLuint uniformLocation = getUniformLocation(key);
+  if (uniformLocation == -1) {
+    return;
+  }
+  glCall(glUniform4f(uniformLocation, v0, v1, v2, v3));
+}
+
+void Shader::setUniform4f(std::string const& key, math::vec4 const& v) {
+  GLuint uniformLocation = getUniformLocation(key);
+  if (uniformLocation == -1) {
+    return;
+  }
+  glCall(glUniform4f(uniformLocation, v[0], v[1], v[2], v[3]));
+}
+
+void Shader::setUniformMat4(std::string const& key, math::mat4 const& m) {
+  GLuint uniformLocation = getUniformLocation(key);
+  if (uniformLocation == -1) {
+    return;
+  }
+  glCall(glUniformMatrix4fv(uniformLocation, 1, GL_FALSE, &m[0][0]));
+}
+
 void Shader::bind() const {
   glCall(glUseProgram(m_Id));
 }
@@ -60,6 +125,21 @@ void Shader::unbind() const {
 
 GLuint Shader::id() const {
   return m_Id;
+}
+
+GLuint Shader::getUniformLocation(std::string const& key) {
+  auto search = m_UniformLocationCache.find(key);
+  if (search != m_UniformLocationCache.end()) {
+    return search->second;
+  }
+  glCall(GLuint uniformLocation = glGetUniformLocation(m_Id, key.c_str()));
+  if (uniformLocation == -1) {
+    // If an uniform is unused (inside a shader code), it is removed.
+    // Therefore glGetUniformLocation could return -1 even if the uniform is actually present.
+    std::cout << "[WebGL error] Could not find uniform \"" << key << "\" location." << std::endl;
+  }
+  m_UniformLocationCache[key] = uniformLocation;
+  return uniformLocation;
 }
 
 }  // namespace engine
--- src/cpp/engine/Shader.hpp
+++ src/cpp/engine/Shader.hpp
@@ -3,6 +3,9 @@
 #include <GLES3/gl3.h>
 
 #include <string>
+#include <unordered_map>
+
+#include "../math/matrix.hpp"
 
 namespace engine {
 
@@ -11,13 +14,25 @@ class Shader {
   GLuint m_Id;
   GLuint m_VertId;
   GLuint m_FragId;
+  std::unordered_map<std::string, GLuint> m_UniformLocationCache;
 
  public:
   Shader(std::string const& vertSrc, std::string const& fragSrc);
   ~Shader();
+  void setUniform1f(std::string const& key, GLfloat v0);
+  void setUniform2f(std::string const& key, GLfloat v0, GLfloat v1);
+  void setUniform2f(std::string const& key, math::vec2 const& v);
+  void setUniform3f(std::string const& key, GLfloat v0, GLfloat v1, GLfloat v2);
+  void setUniform3f(std::string const& key, math::vec3 const& v);
+  void setUniform4f(std::string const& key, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3);
+  void setUniform4f(std::string const& key, math::vec4 const& v);
+  void setUniformMat4(std::string const& key, math::mat4 const& m);
   void bind() const;
   void unbind() const;
   GLuint id() const;
+
+ private:
+  GLuint getUniformLocation(std::string const& key);
 };
 
 }  // namespace engine
