2f8974dada130086fbc2bc3a86d46203d5ba1732
jcancelli <jonathan.cancelli.46@gmail.com>
2023-04-16 19:09:57 +0000

    add type declaration for wasm module

---
+++ src/frontend/lib/wasm/OrbitalsWasmModule.ts
@@ -0,0 +1,13 @@
+// Type declarations for the cpp stuff (classes, functions, etc) exported by the emscripten wasm module
+// Must be updated manually
+
+export type OrbitalsWasmModule = {
+	Orbitals: new () => Orbitals
+} & EmscriptenModule
+
+export abstract class Orbitals {
+	abstract setViewportWidth(width: number): void;
+	abstract setViewportHeight(height: number): void;
+}
+
+export default OrbitalsWasmModule
