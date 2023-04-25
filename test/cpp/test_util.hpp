5563cc33f1d4f00e67aaef57aaf671706cabff01
jcancelli <jonathan.cancelli.46@gmail.com>
2023-04-25 17:00:50 +0000

    Implement CHECK_MAT_EQUALS

--- test/cpp/test_util.hpp
+++ test/cpp/test_util.hpp
@@ -3,6 +3,7 @@
 #include <type_traits>
 
 #include "doctest.h"
+#include "math/matrix.hpp"
 #include "math/vector.hpp"
 
 using orbitals::math::vec;
@@ -13,5 +14,16 @@ inline void CHECK_VEC_EQUALS(vec<vecSize, T> const& v1, vec<vecSize, T> const& v
   for (int i = 0; i < vecSize; i++) {
     INFO("i: ", i);
     CHECK(v1[i] == doctest::Approx(v2[i]));
+  }
+}
+
+template <std::size_t tRows, std::size_t tCols, class T>
+inline void CHECK_MAT_EQUALS(mat<tRows, tCols, T> const& m1, mat<tRows, tCols, T> const& m2) {
+  INFO("Comparing ", m1, " == ", m2);
+  for (int row = 0; row < tRows; row++) {
+    for (int col = 0; col < tCols; col++) {
+      INFO("row: ", row, ", col: ", col);
+      CHECK(m1(col, row) == doctest::Approx(m2(col, row)));
+    }
   }
 }
