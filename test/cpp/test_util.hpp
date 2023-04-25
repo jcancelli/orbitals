3c25fcf8fb39c07f8dfb96273dbe3c0e6be0166e
jcancelli <jonathan.cancelli.46@gmail.com>
2023-04-25 16:57:07 +0000

    Add logging to CHECK_VEC_EQUALS

--- test/cpp/test_util.hpp
+++ test/cpp/test_util.hpp
@@ -9,7 +9,9 @@ using orbitals::math::vec;
 
 template <std::size_t vecSize, class T>
 inline void CHECK_VEC_EQUALS(vec<vecSize, T> const& v1, vec<vecSize, T> const& v2) {
+  INFO("Comparing ", v1, " == ", v2);
   for (int i = 0; i < vecSize; i++) {
+    INFO("i: ", i);
     CHECK(v1[i] == doctest::Approx(v2[i]));
   }
 }
