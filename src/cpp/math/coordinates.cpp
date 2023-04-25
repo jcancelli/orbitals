e312466c3a2d9a1a16def079c6d41a8ec6cb7642
jcancelli <jonathan.cancelli.46@gmail.com>
2023-04-25 18:54:41 +0000

    Improve cartesian to spherical coordinates conversion

--- src/cpp/math/coordinates.cpp
+++ src/cpp/math/coordinates.cpp
@@ -7,26 +7,58 @@ namespace orbitals {
 
 namespace math {
 
+/*
+    zenith direction: positive y axis
+    inclination: angle [0°-180°] between the positive y axis and the position vector
+    azimuth: angle [0°-180°] of rotation around the y axis. 0° is on the positive z axis, 180° is
+                on the negative z axis
+*/
+
+static float inclination(float x, float y, float z) {
+  if (y > 0) {
+    return atan(hypot(z, x) / y);
+  }
+  if (y < 0) {
+    return M_PI + atan(hypot(z, x) / y);
+  }
+  return M_PI_2;
+}
+
+static float azimuth(float x, float y, float z) {
+  if (z > 0) {
+    return atan(x / z);
+  }
+  if (z < 0) {
+    if (x >= 0) {
+      return atan(x / z) + M_PI;
+    }
+    return atan(x / z) - M_PI;
+  }
+  if (x > 0) {
+    return M_PI_2;
+  }
+  return -M_PI_2;
+}
+
 vec3 spherical(float x, float y, float z) {
-  assert(x != 0 || y != 0);
-  const float r = sqrt(x * x + y * y + z * z);
-  return vec3(                                     //
-      r,                                           //
-      acos(z / r),                                 //
-      ((y > 0) - (y < 0)) * acos(x / hypot(x, y))  //
-  );                                               //
+  assert(z != 0 || x != 0);
+  return vec3(                      //
+      sqrt(x * x + y * y + z * z),  // r
+      inclination(x, y, z),         // inclination
+      azimuth(x, y, z)              // azimuth
+  );                                //
 }
 
 vec3 spherical(vec3 const& cart) {
   return spherical(cart.x(), cart.y(), cart.z());
 }
 
-vec3 cartesian(float r, float theta, float phi) {
-  return vec3(                    //
-      r * sin(phi) * cos(theta),  //
-      r * sin(phi) * sin(theta),  //
-      r * cos(phi)                //
-  );                              //
+vec3 cartesian(float r, float inclination, float azimuth) {
+  return vec3(                              //
+      r * cos(azimuth),                     // x
+      r * sin(azimuth) * cos(inclination),  // y
+      r * sin(azimuth) * sin(inclination)   // z
+  );                                        //
 }
 
 vec3 cartesian(vec3 const& sphe) {
