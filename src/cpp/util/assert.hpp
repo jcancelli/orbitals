c82b266271039f87ce7f7ad772479b775792a378
jcancelli <jonathan.cancelli.46@gmail.com>
2023-05-02 21:03:41 +0000

    Implement custom assert

---
+++ src/cpp/util/assert.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+
+#define ASSERT(condition, message)                                                           \
+  if (!(condition)) {                                                                        \
+    std::cerr << "----------\n[Failed assert]\nFile: " << __FILE__ << "\nLine: " << __LINE__ \
+              << "\nMessage: " << message << "\n----------\n";                               \
+    assert(false);                                                                           \
+  }
