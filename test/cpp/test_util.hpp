21fe39d129b40e940a8e3b1f7145147dae51a384
jcancelli <jonathan.cancelli.46@gmail.com>
2023-05-04 17:40:17 +0000

    Add epsilon parameter to test util functions

--- test/cpp/test_util.hpp
+++ test/cpp/test_util.hpp
@@ -11,28 +11,31 @@ using orbitals::math::mat;
 using orbitals::math::vec;
 
 template <std::size_t vecSize, class T>
-inline void CHECK_VEC_EQUALS(vec<vecSize, T> const& v1, vec<vecSize, T> const& v2) {
+inline void CHECK_VEC_EQUALS(vec<vecSize, T> const& v1, vec<vecSize, T> const& v2,
+                             float epsilon = .00001) {
   INFO("Comparing ", v1, " == ", v2);
   for (int i = 0; i < vecSize; i++) {
     INFO("i: ", i);
-    CHECK(v1[i] == doctest::Approx(v2[i]));
+    CHECK(v1[i] == doctest::Approx(v2[i]).epsilon(epsilon));
   }
 }
 
 template <std::size_t tRows, std::size_t tCols, class T>
-inline void CHECK_MAT_EQUALS(mat<tRows, tCols, T> const& m1, mat<tRows, tCols, T> const& m2) {
+inline void CHECK_MAT_EQUALS(mat<tRows, tCols, T> const& m1, mat<tRows, tCols, T> const& m2,
+                             float epsilon = .00001) {
   INFO("Comparing ", m1, " == ", m2);
   for (int row = 0; row < tRows; row++) {
     for (int col = 0; col < tCols; col++) {
       INFO("row: ", row, ", col: ", col);
-      CHECK(m1(col, row) == doctest::Approx(m2(col, row)));
+      CHECK(m1(col, row) == doctest::Approx(m2(col, row)).epsilon(epsilon));
     }
   }
 }
 
 template <class T>
-inline void CHECK_COMPLEX_EQUALS(std::complex<T> const& a, std::complex<T> const& b) {
+inline void CHECK_COMPLEX_EQUALS(std::complex<T> const& a, std::complex<T> const& b,
+                                 float epsilon = .00001) {
   INFO("Comparing ", a, " == ", b);
-  CHECK(a.real() == doctest::Approx(b.real()));
-  CHECK(a.imag() == doctest::Approx(b.imag()));
+  CHECK(a.real() == doctest::Approx(b.real()).epsilon(epsilon));
+  CHECK(a.imag() == doctest::Approx(b.imag()).epsilon(epsilon));
 }
