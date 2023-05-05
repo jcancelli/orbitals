15934a431cd995d4daafb4b818d51bf33c6a93c5
jcancelli <jonathan.cancelli.46@gmail.com>
2023-05-05 20:39:53 +0000

    Add sign checking to radial function tests

--- test/cpp/math/radial_function.test.hpp
+++ test/cpp/math/radial_function.test.hpp
@@ -26,6 +26,7 @@ TEST_CASE("Testing radial function") {
           R.setL(0);
           const double expected = 2. * exp(-r);
           CHECK(R() == doctest::Approx(expected).epsilon(.000001));
+          CHECK_SIGN_EQUALS(R(), expected);
         }
       }
 
@@ -36,12 +37,14 @@ TEST_CASE("Testing radial function") {
           R.setL(0);
           const double expected = (1 / sqrt(2)) * (1 - (r / 2)) * exp(-r / 2);
           CHECK(R() == doctest::Approx(expected).epsilon(.000001));
+          CHECK_SIGN_EQUALS(R(), expected);
         }
 
         SUBCASE("l: 1") {
           R.setL(1);
           const double expected = (1 / sqrt(24)) * r * exp(-r / 2);
           CHECK(R() == doctest::Approx(expected).epsilon(.000001));
+          CHECK_SIGN_EQUALS(R(), expected);
         }
       }
 
@@ -53,18 +56,21 @@ TEST_CASE("Testing radial function") {
           const double expected =
               (2 / sqrt(27)) * (1 - ((2 * r) / 3) + ((2 * pow(r, 2)) / 27)) * exp(-r / 3);
           CHECK(R() == doctest::Approx(expected).epsilon(.000001));
+          CHECK_SIGN_EQUALS(R(), expected);
         }
 
         SUBCASE("l: 1") {
           R.setL(1);
           const double expected = (8 / (27 * sqrt(6))) * r * (1 - (r / 6)) * exp(-r / 3);
           CHECK(R() == doctest::Approx(expected).epsilon(.000001));
+          CHECK_SIGN_EQUALS(R(), expected);
         }
 
         SUBCASE("l: 2") {
           R.setL(2);
           const double expected = (4 / (81 * sqrt(30))) * pow(r, 2) * exp(-r / 3);
           CHECK(R() == doctest::Approx(expected).epsilon(.000001));
+          CHECK_SIGN_EQUALS(R(), expected);
         }
       }
 
@@ -78,6 +84,7 @@ TEST_CASE("Testing radial function") {
           const double expected =
               (1. / 96.) * (24 - 36 * roh + 12 * pow(roh, 2) - pow(roh, 3)) * exp(-r / 4);
           CHECK(R() == doctest::Approx(expected).epsilon(.000001));
+          CHECK_SIGN_EQUALS(R(), expected);
         }
 
         SUBCASE("l: 1") {
@@ -85,18 +92,21 @@ TEST_CASE("Testing radial function") {
           const double expected =
               (1 / (32 * sqrt(15))) * (20 - 10 * roh + pow(roh, 2)) * roh * exp(-r / 4);
           CHECK(R() == doctest::Approx(expected).epsilon(.000001));
+          CHECK_SIGN_EQUALS(R(), expected);
         }
 
         SUBCASE("l: 2") {
           R.setL(2);
           const double expected = (1 / (96 * sqrt(5))) * (6 - roh) * pow(roh, 2) * exp(-r / 4);
           CHECK(R() == doctest::Approx(expected).epsilon(.000001));
+          CHECK_SIGN_EQUALS(R(), expected);
         }
 
         SUBCASE("l: 3") {
           R.setL(3);
           const double expected = (1 / (96 * sqrt(35))) * pow(roh, 3) * exp(-r / 4);
           CHECK(R() == doctest::Approx(expected).epsilon(.000001));
+          CHECK_SIGN_EQUALS(R(), expected);
         }
       }
     }
