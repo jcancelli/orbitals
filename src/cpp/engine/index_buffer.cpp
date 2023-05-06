59cc99c956719e7b6af58328d273cf3983fea9eb
jcancelli <jonathan.cancelli.46@gmail.com>
2023-05-06 14:17:29 +0000

    Add getPrimitivesCount to IndexBuffer. Renamed IndexBuffer to index_buffer

--- src/cpp/engine/index_buffer.cpp
+++ src/cpp/engine/index_buffer.cpp
@@ -1,12 +1,15 @@
 #include "IndexBuffer.hpp"
 
+#include <cassert>
+
 #include "glDebug.hpp"
 
 namespace orbitals {
 
 namespace engine {
 
-IndexBuffer::IndexBuffer(std::vector<GLuint> const& indices, GLenum usage = GL_STATIC_DRAW) {
+IndexBuffer::IndexBuffer(std::vector<GLuint> const& indices, GLenum usage = GL_STATIC_DRAW)
+    : m_Count{indices.size()} {
   glCall(glGenBuffers(1, &m_Id));
   glCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * indices.size(), indices.data(),
                       usage));
@@ -14,6 +17,23 @@ IndexBuffer::IndexBuffer(std::vector<GLuint> const& indices, GLenum usage = GL_S
 
 IndexBuffer::~IndexBuffer() {
   glCall(glDeleteBuffers(1, &m_Id));
+}
+
+GLsizei IndexBuffer::getPrimitivesCount(GLenum type) const {
+  switch (type) {
+    case GL_TRIANGLES:
+      assert(m_Count % 3 == 0);
+      return m_Count / 3;
+    case GL_LINES:
+      assert(m_Count % 2 == 0);
+      return m_Count / 2;
+    case GL_POINTS:
+      return m_Count;
+  }
+}
+
+std::size_t IndexBuffer::getCount() const {
+  return m_Count;
 }
 
 void IndexBuffer::bind() const {
--- src/cpp/engine/index_buffer.hpp
+++ src/cpp/engine/index_buffer.hpp
@@ -11,10 +11,13 @@ namespace engine {
 class IndexBuffer {
  private:
   GLuint m_Id;
+  std::size_t m_Count;
 
  public:
   IndexBuffer(std::vector<GLuint> const& indices, GLenum usage);
   ~IndexBuffer();
+  GLsizei getPrimitivesCount(GLenum type) const;
+  std::size_t getCount() const;
   void bind() const;
   void unbind() const;
   GLuint id() const;
