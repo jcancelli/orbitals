451ca6183005e9043a3cf5312eeb51617d2b4c44
jcancelli <jonathan.cancelli.46@gmail.com>
2023-05-08 14:03:01 +0000

    Init HTMLCanvasViewport size on creation

--- src/cpp/engine/viewport.cpp
+++ src/cpp/engine/viewport.cpp
@@ -1,8 +1,7 @@
 #include "viewport.hpp"
 
+#include <emscripten/emscripten.h>
 #include <emscripten/html5.h>
-
-#include <iostream>
 
 namespace orbitals {
 
@@ -28,17 +27,23 @@ float Viewport::aspectRatio() const {
   return m_Width / m_Height;
 }
 
+EM_JS(int, _getHTMLCanvasWidth, (char const *HTMLCanvasId),
+      { return document.getElementById(UTF8ToString(HTMLCanvasId)).width; });
+EM_JS(int, _getHTMLCanvasHeight, (char const *HTMLCanvasId),
+      { return document.getElementById(UTF8ToString(HTMLCanvasId)).height; });
+
 HTMLCanvasViewport::HTMLCanvasViewport(std::string const &canvasHTMLId)
     : m_CanvasHTMLId(canvasHTMLId) {
   emscripten_set_resize_callback(
-      EMSCRIPTEN_EVENT_TARGET_DOCUMENT, this, EM_FALSE,
+      EMSCRIPTEN_EVENT_TARGET_WINDOW, this, EM_FALSE,
       [](int eventType, const EmscriptenUiEvent *uiEvent, void *userData) {
         Viewport *vp = (Viewport *)userData;
         vp->setHeight(uiEvent->windowInnerHeight);
         vp->setWidth(uiEvent->windowInnerWidth);
-        std::cout << vp->getHeight() << " " << vp->getWidth() << "\n";
         return EM_FALSE;
       });
+  setWidth(_getHTMLCanvasWidth(canvasHTMLId.c_str()));
+  setHeight(_getHTMLCanvasHeight(canvasHTMLId.c_str()));
 }
 HTMLCanvasViewport::~HTMLCanvasViewport() {
   emscripten_set_resize_callback(EMSCRIPTEN_EVENT_TARGET_DOCUMENT, this, EM_FALSE, nullptr);
