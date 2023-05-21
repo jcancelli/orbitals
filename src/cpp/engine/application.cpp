81de178c9837576e031d4dd89d33c4654b1644e5
jcancelli <jonathan.cancelli.46@gmail.com>
2023-05-21 23:00:19 +0000

    Add Application first implementation

---
+++ src/cpp/engine/application.cpp
@@ -0,0 +1,127 @@
+#include "application.hpp"
+
+#include <GLES3/gl3.h>
+#include <emscripten.h>
+#include <emscripten/bind.h>
+#include <emscripten/html5.h>
+
+#include "../imgui/imgui.h"
+#include "../imgui/imgui_impl_opengl3.h"
+#include "debug.hpp"
+
+namespace orbitals {
+
+namespace engine {
+
+Application::Application(std::string const& canvasID) {
+  init(canvasID);
+}
+
+void Application::start() {
+  m_IsRunning = EM_TRUE;
+  emscripten_request_animation_frame_loop(cycleWrapper, this);
+}
+
+void Application::stop() {
+  m_IsRunning = EM_FALSE;
+}
+
+void Application::setScene(std::shared_ptr<Scene> scene) {
+  m_Scene = scene;
+}
+
+std::shared_ptr<Viewport const> Application::getViewport() const {
+  return m_Viewport;
+}
+
+std::shared_ptr<Clock const> Application::getClock() const {
+  return m_Clock;
+}
+
+std::shared_ptr<Inputs> Application::getInputs() {
+  return m_Inputs;
+}
+
+void Application::onInit() {
+}
+
+void Application::onUpdate() {
+}
+
+void Application::onShutdown() {
+}
+
+void Application::init(std::string const& canvasID) {
+  // Init WebGL context
+  using namespace std::string_literals;  // for ""s operator
+  EmscriptenWebGLContextAttributes ctxAttributes;
+  emscripten_webgl_init_context_attributes(&ctxAttributes);
+  m_WebGLContext = emscripten_webgl_create_context(("#"s + canvasID).c_str(), &ctxAttributes);
+  emscripten_webgl_make_context_current(m_WebGLContext);
+  std::cout << "--------------------------------------------------\n";
+  std::cout << glGetString(GL_VERSION) << "\n";
+  glCall(glEnable(GL_DEPTH_TEST));
+
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGui::StyleColorsDark();
+  ImGui_ImplOpenGL3_Init("#version 300 es");
+
+  // Setup viewport
+  m_Viewport = std::shared_ptr<Viewport>(new HTMLCanvasViewport(canvasID));
+  m_Viewport->addResizeListener([](float width, float height) {
+    ImGui::GetIO().DisplaySize.x = width;
+    ImGui::GetIO().DisplaySize.y = height;
+  });
+
+  // Setup inputs
+  m_Inputs = std::shared_ptr<Inputs>(new Inputs);
+  m_Inputs->addKeyDownListener(
+      [](Key key) { ImGui::GetIO().AddKeyEvent(keyToImGuiKey(key), true); });
+  m_Inputs->addKeyUpListener(
+      [](Key key) { ImGui::GetIO().AddKeyEvent(keyToImGuiKey(key), false); });
+  m_Inputs->addMouseDownListener(
+      [](Mouse::Button button) { ImGui::GetIO().AddMouseButtonEvent(button, true); });
+  m_Inputs->addMouseUpListener(
+      [](Mouse::Button button) { ImGui::GetIO().AddMouseButtonEvent(button, false); });
+  m_Inputs->addMouseMoveListener([](float x, float y) { ImGui::GetIO().AddMousePosEvent(x, y); });
+  m_Inputs->addWheelListener([](float scroll) { ImGui::GetIO().AddMouseWheelEvent(0, scroll); });
+
+  onInit();
+}
+
+void Application::shutdown() {
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui::DestroyContext();
+  onShutdown();
+}
+
+EM_BOOL Application::cycle(double elapsed) {
+  m_Clock->tick(elapsed);
+  m_Scene->handleInput(m_Inputs->getEvents(), m_Inputs->getKeyboard(), m_Inputs->getMouse());
+  m_Scene->update(m_Clock->delta());
+  onUpdate();
+  glCall(glClearColor(1, 1, 1, 1));
+  glCall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
+  m_Scene->draw(m_Viewport);
+  ImGui::Render();
+  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+  m_Inputs->clear();
+  return m_IsRunning;
+}
+
+EM_BOOL Application::cycleWrapper(double elapsed, void* userData) {
+  return ((Application*)userData)->cycle(elapsed);
+}
+
+EMSCRIPTEN_BINDINGS(orbitals) {
+  emscripten::class_<Application>("Application")
+      .constructor<std::string>()
+      .function("start", &Application::start)
+      .function("stop", &Application::stop);
+}
+
+}  // namespace engine
+
+}  // namespace orbitals
---
+++ src/cpp/engine/application.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <emscripten/html5.h>
+
+#include <memory>
+
+#include "clock.hpp"
+#include "inputs.hpp"
+#include "scene.hpp"
+#include "viewport.hpp"
+
+namespace orbitals {
+
+namespace engine {
+
+class Application {
+ private:
+  EMSCRIPTEN_WEBGL_CONTEXT_HANDLE m_WebGLContext;
+  EM_BOOL m_IsRunning;
+  std::shared_ptr<Viewport> m_Viewport;
+  std::shared_ptr<Clock> m_Clock;
+  std::shared_ptr<Inputs> m_Inputs;
+  std::shared_ptr<Scene> m_Scene;
+
+ public:
+  Application(std::string const& canvasID);
+  void start();
+  void stop();
+  void setScene(std::shared_ptr<Scene> scene);
+  std::shared_ptr<Viewport const> getViewport() const;
+  std::shared_ptr<Clock const> getClock() const;
+  std::shared_ptr<Inputs> getInputs();
+  virtual void onInit();
+  virtual void onUpdate();
+  virtual void onShutdown();
+
+ private:
+  void init(std::string const& canvasID);
+  void shutdown();
+  EM_BOOL cycle(double elapsed);
+  static EM_BOOL cycleWrapper(double elapsed, void* userData);
+};
+
+}  // namespace engine
+
+}  // namespace orbitals
