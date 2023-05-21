6df8392c63e10899540a01078554733a42f9fd3f
jcancelli <jonathan.cancelli.46@gmail.com>
2023-05-21 21:25:40 +0000

    Add methods for adding/removing inputs listeners

--- src/cpp/engine/inputs.cpp
+++ src/cpp/engine/inputs.cpp
@@ -1,87 +1,136 @@
 #include "inputs.hpp"
 
-#include <boost/algorithm/string.hpp>
+#include <iostream>
 
 namespace orbitals {
 
 namespace engine {
 
-EM_BOOL Inputs::keyboard_event_callback(int type, const EmscriptenKeyboardEvent* emEvent,
+EM_BOOL Inputs::key_down_callback(int eventType, const EmscriptenKeyboardEvent* emEvent,
+                                  void* userData) {
+  Inputs* inputs = (Inputs*)userData;
+  Inputs::Event event = {.type = (Inputs::Event::Type)eventType, .keyboard = *emEvent};
+  Key key = browserStringToKey(emEvent->key);
+  inputs->m_Keyboard.press(key);
+  inputs->push(event);
+  inputs->m_KeyDownListeners.notify(key);
+  return EM_FALSE;
+}
+
+EM_BOOL Inputs::key_up_callback(int eventType, const EmscriptenKeyboardEvent* emEvent,
+                                void* userData) {
+  Inputs* inputs = (Inputs*)userData;
+  Inputs::Event event = {.type = (Inputs::Event::Type)eventType, .keyboard = *emEvent};
+  Key key = browserStringToKey(emEvent->key);
+  inputs->m_Keyboard.release(key);
+  inputs->push(event);
+  inputs->m_KeyUpListeners.notify(key);
+  return EM_FALSE;
+}
+
+EM_BOOL Inputs::key_pressed_callback(int eventType, const EmscriptenKeyboardEvent* emEvent,
+                                     void* userData) {
+  Inputs* inputs = (Inputs*)userData;
+  Inputs::Event event = {.type = (Inputs::Event::Type)eventType, .keyboard = *emEvent};
+  Key key = browserStringToKey(emEvent->key);
+  inputs->push(event);
+  return EM_FALSE;
+}
+
+EM_BOOL Inputs::mouse_click_callback(int eventType, const EmscriptenMouseEvent* emEvent,
+                                     void* userData) {
+  Inputs* inputs = (Inputs*)userData;
+  Inputs::Event event = {.type = (Inputs::Event::Type)eventType, .mouse = *emEvent};
+  inputs->push(event);
+  return EM_FALSE;
+}
+
+EM_BOOL Inputs::mouse_down_callback(int eventType, const EmscriptenMouseEvent* emEvent,
+                                    void* userData) {
+  Inputs* inputs = (Inputs*)userData;
+  Inputs::Event event = {.type = (Inputs::Event::Type)eventType, .mouse = *emEvent};
+  Mouse::Button button = (Mouse::Button)emEvent->button;
+  inputs->m_Mouse.press(button);
+  inputs->push(event);
+  inputs->m_MouseDownListeners.notify(button);
+  return EM_FALSE;
+}
+
+EM_BOOL Inputs::mouse_up_callback(int eventType, const EmscriptenMouseEvent* emEvent,
+                                  void* userData) {
+  Inputs* inputs = (Inputs*)userData;
+  Inputs::Event event = {.type = (Inputs::Event::Type)eventType, .mouse = *emEvent};
+  Mouse::Button button = (Mouse::Button)emEvent->button;
+  inputs->m_Mouse.release(button);
+  inputs->push(event);
+  inputs->m_MouseUpListeners.notify(button);
+  return EM_FALSE;
+}
+
+EM_BOOL Inputs::mouse_dblclick_callback(int eventType, const EmscriptenMouseEvent* emEvent,
                                         void* userData) {
   Inputs* inputs = (Inputs*)userData;
-  Inputs::Event event = {.type = (Inputs::Event::Type)type, .keyboard = *emEvent};
-
-  switch (event.type) {
-    case Inputs::Event::Type::KeyDown:
-      inputs->m_Keyboard.press(browserStringToKey(emEvent->key));
-      break;
-    case Inputs::Event::Type::KeyUp:
-      inputs->m_Keyboard.release(browserStringToKey(emEvent->key));
-      break;
-    default:
-      // do nothing
-      break;
-  }
-
+  Inputs::Event event = {.type = (Inputs::Event::Type)eventType, .mouse = *emEvent};
   inputs->push(event);
   return EM_FALSE;
 }
 
-EM_BOOL Inputs::mouse_event_callback(int type, const EmscriptenMouseEvent* emEvent,
+EM_BOOL Inputs::mouse_move_callback(int eventType, const EmscriptenMouseEvent* emEvent,
+                                    void* userData) {
+  Inputs* inputs = (Inputs*)userData;
+  Inputs::Event event = {.type = (Inputs::Event::Type)eventType, .mouse = *emEvent};
+  inputs->push(event);
+  inputs->m_MouseMoveListeners.notify(emEvent->clientX, emEvent->clientY);
+  return EM_FALSE;
+}
+
+EM_BOOL Inputs::mouse_enter_callback(int eventType, const EmscriptenMouseEvent* emEvent,
                                      void* userData) {
   Inputs* inputs = (Inputs*)userData;
-  Inputs::Event event = {.type = (Inputs::Event::Type)type, .mouse = *emEvent};
-
-  switch (event.type) {
-    case Inputs::Event::Type::MouseDown:
-      inputs->m_Mouse.press((Mouse::Button)emEvent->button);
-      break;
-    case Inputs::Event::Type::MouseUp:
-      inputs->m_Mouse.release((Mouse::Button)emEvent->button);
-      break;
-    case Inputs::Event::Type::MouseMove:
-      inputs->m_Mouse.setPosition(emEvent->clientX, emEvent->clientY);
-      break;
-    default:
-      // do nothing
-      break;
-  }
-
+  Inputs::Event event = {.type = (Inputs::Event::Type)eventType, .mouse = *emEvent};
   inputs->push(event);
   return EM_FALSE;
 }
 
-EM_BOOL Inputs::wheel_event_callback(int type, const EmscriptenWheelEvent* emEvent,
+EM_BOOL Inputs::mouse_leave_callback(int eventType, const EmscriptenMouseEvent* emEvent,
                                      void* userData) {
   Inputs* inputs = (Inputs*)userData;
-  Inputs::Event event = {.type = (Inputs::Event::Type)type, .wheel = *emEvent};
+  Inputs::Event event = {.type = (Inputs::Event::Type)eventType, .mouse = *emEvent};
   inputs->push(event);
+  return EM_FALSE;
+}
+
+EM_BOOL Inputs::wheel_event_callback(int eventType, const EmscriptenWheelEvent* emEvent,
+                                     void* userData) {
+  Inputs* inputs = (Inputs*)userData;
+  Inputs::Event event = {.type = (Inputs::Event::Type)eventType, .wheel = *emEvent};
+  inputs->push(event);
+  inputs->m_WheelListeners.notify(emEvent->deltaY);
   return EM_FALSE;
 }
 
 Inputs::Inputs() {
   // keyboard
   emscripten_set_keypress_callback(EMSCRIPTEN_EVENT_TARGET_DOCUMENT, this, EM_FALSE,
-                                   keyboard_event_callback);
+                                   key_pressed_callback);
   emscripten_set_keydown_callback(EMSCRIPTEN_EVENT_TARGET_DOCUMENT, this, EM_FALSE,
-                                  keyboard_event_callback);
-  emscripten_set_keyup_callback(EMSCRIPTEN_EVENT_TARGET_DOCUMENT, this, EM_FALSE,
-                                keyboard_event_callback);
+                                  key_down_callback);
+  emscripten_set_keyup_callback(EMSCRIPTEN_EVENT_TARGET_DOCUMENT, this, EM_FALSE, key_up_callback);
   // mouse
   emscripten_set_click_callback(EMSCRIPTEN_EVENT_TARGET_DOCUMENT, this, EM_FALSE,
-                                mouse_event_callback);
+                                mouse_click_callback);
   emscripten_set_mousedown_callback(EMSCRIPTEN_EVENT_TARGET_DOCUMENT, this, EM_FALSE,
-                                    mouse_event_callback);
+                                    mouse_down_callback);
   emscripten_set_mouseup_callback(EMSCRIPTEN_EVENT_TARGET_DOCUMENT, this, EM_FALSE,
-                                  mouse_event_callback);
+                                  mouse_up_callback);
   emscripten_set_dblclick_callback(EMSCRIPTEN_EVENT_TARGET_DOCUMENT, this, EM_FALSE,
-                                   mouse_event_callback);
+                                   mouse_dblclick_callback);
   emscripten_set_mousemove_callback(EMSCRIPTEN_EVENT_TARGET_DOCUMENT, this, EM_FALSE,
-                                    mouse_event_callback);
+                                    mouse_move_callback);
   emscripten_set_mouseenter_callback(EMSCRIPTEN_EVENT_TARGET_DOCUMENT, this, EM_FALSE,
-                                     mouse_event_callback);
+                                     mouse_enter_callback);
   emscripten_set_mouseleave_callback(EMSCRIPTEN_EVENT_TARGET_DOCUMENT, this, EM_FALSE,
-                                     mouse_event_callback);
+                                     mouse_leave_callback);
   // wheel
   emscripten_set_wheel_callback(EMSCRIPTEN_EVENT_TARGET_DOCUMENT, this, EM_FALSE,
                                 wheel_event_callback);
@@ -102,6 +151,62 @@ Inputs::~Inputs() {
   emscripten_set_mouseleave_callback(EMSCRIPTEN_EVENT_TARGET_DOCUMENT, nullptr, EM_FALSE, nullptr);
   // wheel
   emscripten_set_wheel_callback(EMSCRIPTEN_EVENT_TARGET_DOCUMENT, nullptr, EM_FALSE, nullptr);
+}
+
+unsigned Inputs::addKeyDownListener(util::Listeners<Key>::Listener const& listener) {
+  return m_KeyDownListeners.add(listener);
+}
+
+void Inputs::removeKeyDownListener(unsigned listenerID) {
+  m_KeyDownListeners.remove(listenerID);
+}
+
+unsigned Inputs::addKeyUpListener(util::Listeners<Key>::Listener const& listener) {
+  return m_KeyUpListeners.add(listener);
+}
+
+void Inputs::removeKeyUpListener(unsigned listenerID) {
+  m_KeyUpListeners.remove(listenerID);
+}
+
+unsigned Inputs::addMouseUpListener(util::Listeners<Mouse::Button>::Listener const& listener) {
+  return m_MouseUpListeners.add(listener);
+}
+
+void Inputs::removeMouseUpListener(unsigned listenerID) {
+  m_MouseUpListeners.remove(listenerID);
+}
+
+unsigned Inputs::addMouseDownListener(util::Listeners<Mouse::Button>::Listener const& listener) {
+  return m_KeyDownListeners.add(listener);
+}
+
+void Inputs::removeMouseDownListener(unsigned listenerID) {
+  m_KeyDownListeners.remove(listenerID);
+}
+
+unsigned Inputs::addMouseMoveListener(util::Listeners<float, float>::Listener const& listener) {
+  return m_MouseMoveListeners.add(listener);
+}
+
+void Inputs::removeMouseMoveListener(unsigned listenerID) {
+  m_MouseMoveListeners.remove(listenerID);
+}
+
+unsigned Inputs::addWheelListener(util::Listeners<float>::Listener const& listener) {
+  return m_WheelListeners.add(listener);
+}
+
+void Inputs::removeWheelListener(unsigned listenerID) {
+  m_WheelListeners.remove(listenerID);
+}
+unsigned Inputs::addOnNewFrameListener(
+    util::Listeners<std::vector<Event>, Keyboard const&, Mouse const&>::Listener const& listener) {
+  return m_OnNewFrameListeners.add(listener);
+}
+
+void Inputs::removeOnNewFrameListener(unsigned listenerID) {
+  m_OnNewFrameListeners.remove(listenerID);
 }
 
 void Inputs::clear() {
--- src/cpp/engine/inputs.hpp
+++ src/cpp/engine/inputs.hpp
@@ -4,6 +4,7 @@
 
 #include <vector>
 
+#include "../util/listeners_manager.hpp"
 #include "keyboard.hpp"
 #include "mouse.hpp"
 
@@ -41,10 +42,32 @@ class Inputs {
   Keyboard m_Keyboard;
   Mouse m_Mouse;
   std::vector<Event> m_Events;
+  util::Listeners<Key> m_KeyDownListeners;
+  util::Listeners<Key> m_KeyUpListeners;
+  util::Listeners<Mouse::Button> m_MouseDownListeners;
+  util::Listeners<Mouse::Button> m_MouseUpListeners;
+  util::Listeners<float, float> m_MouseMoveListeners;
+  util::Listeners<float> m_WheelListeners;
+  util::Listeners<std::vector<Event>, Keyboard const&, Mouse const&> m_OnNewFrameListeners;
 
  public:
   Inputs();
   ~Inputs();
+  unsigned addKeyDownListener(util::Listeners<Key>::Listener const& listener);
+  void removeKeyDownListener(unsigned listenerID);
+  unsigned addKeyUpListener(util::Listeners<Key>::Listener const& listener);
+  void removeKeyUpListener(unsigned listenerID);
+  unsigned addMouseUpListener(util::Listeners<Mouse::Button>::Listener const& listener);
+  void removeMouseUpListener(unsigned listenerID);
+  unsigned addMouseDownListener(util::Listeners<Mouse::Button>::Listener const& listener);
+  void removeMouseDownListener(unsigned listenerID);
+  unsigned addMouseMoveListener(util::Listeners<float, float>::Listener const& listener);
+  void removeMouseMoveListener(unsigned listenerID);
+  unsigned addWheelListener(util::Listeners<float>::Listener const& listener);
+  void removeWheelListener(unsigned listenerID);
+  unsigned addOnNewFrameListener(
+      util::Listeners<std::vector<Event>, Keyboard const&, Mouse const&>::Listener const& listener);
+  void removeOnNewFrameListener(unsigned listenerID);
   void clear();
   std::vector<Event> const& getEvents() const;
   Keyboard const& getKeyboard() const;
@@ -52,9 +75,25 @@ class Inputs {
 
  private:
   void push(Event event);
-  static EM_BOOL keyboard_event_callback(int eventType, const EmscriptenKeyboardEvent* keyEvent,
+  static EM_BOOL key_down_callback(int eventType, const EmscriptenKeyboardEvent* keyEvent,
+                                   void* userData);
+  static EM_BOOL key_up_callback(int eventType, const EmscriptenKeyboardEvent* keyEvent,
+                                 void* userData);
+  static EM_BOOL key_pressed_callback(int eventType, const EmscriptenKeyboardEvent* keyEvent,
+                                      void* userData);
+  static EM_BOOL mouse_click_callback(int eventType, const EmscriptenMouseEvent* emEvent,
+                                      void* userData);
+  static EM_BOOL mouse_down_callback(int eventType, const EmscriptenMouseEvent* emEvent,
+                                     void* userData);
+  static EM_BOOL mouse_up_callback(int eventType, const EmscriptenMouseEvent* emEvent,
+                                   void* userData);
+  static EM_BOOL mouse_dblclick_callback(int eventType, const EmscriptenMouseEvent* emEvent,
                                          void* userData);
-  static EM_BOOL mouse_event_callback(int emEventType, const EmscriptenMouseEvent* emEvent,
+  static EM_BOOL mouse_move_callback(int eventType, const EmscriptenMouseEvent* emEvent,
+                                     void* userData);
+  static EM_BOOL mouse_enter_callback(int eventType, const EmscriptenMouseEvent* emEvent,
+                                      void* userData);
+  static EM_BOOL mouse_leave_callback(int eventType, const EmscriptenMouseEvent* emEvent,
                                       void* userData);
   static EM_BOOL wheel_event_callback(int emEventType, const EmscriptenWheelEvent* emEvent,
                                       void* userData);
