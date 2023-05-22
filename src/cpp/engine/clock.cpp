9238d676bd87c6d564482d2e04d1e4f4bdbfbf3e
jcancelli <jonathan.cancelli.46@gmail.com>
2023-05-22 18:31:05 +0000

    Add onTickListeners to Clock

--- src/cpp/engine/clock.cpp
+++ src/cpp/engine/clock.cpp
@@ -7,6 +7,7 @@ namespace engine {
 void Clock::tick(double newElapsed) {
   m_Delta = newElapsed - m_Elapsed;
   m_Elapsed = newElapsed;
+  m_OnTickListeners.notify(*this);
 }
 
 double Clock::delta() const {
@@ -15,6 +16,14 @@ double Clock::delta() const {
 
 double Clock::elapsed() const {
   return m_Elapsed;
+}
+
+unsigned Clock::addOnTickListener(util::Listeners<Clock const&>::Listener const& listener) {
+  return m_OnTickListeners.add(listener);
+}
+
+void Clock::removeOnTickListener(unsigned listenerID) {
+  m_OnTickListeners.remove(listenerID);
 }
 
 }  // namespace engine
--- src/cpp/engine/clock.hpp
+++ src/cpp/engine/clock.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../util/listeners_manager.hpp"
+
 namespace orbitals {
 
 namespace engine {
@@ -8,11 +10,14 @@ class Clock {
  private:
   double m_Elapsed;
   double m_Delta;
+  util::Listeners<Clock const&> m_OnTickListeners;
 
  public:
   void tick(double newElapsed);
   double delta() const;
   double elapsed() const;
+  unsigned addOnTickListener(util::Listeners<Clock const&>::Listener const& listener);
+  void removeOnTickListener(unsigned listenerID);
 };
 
 }  // namespace engine
