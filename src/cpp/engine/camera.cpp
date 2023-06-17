4eb54573abd88b8d63a4f2b61096f09341327bcd
jcancelli <jonathan.cancelli.46@gmail.com>
2023-06-17 14:45:43 +0000

    Rework camera

--- src/cpp/engine/camera.cpp
+++ src/cpp/engine/camera.cpp
@@ -1,12 +1,12 @@
 #include "camera.hpp"
 
+#include "../math/projection_matrix.hpp"
 #include "../math/trigonometry.hpp"
 
 namespace orbitals {
 
 namespace engine {
 
-static const float defaultFOV = math::radians(45);
 static const float defaultZNear = 0.1f;
 static const float defaultZFar = 2000.f;
 
@@ -15,9 +15,9 @@ Camera::Camera()
       m_Center(0),
       m_UpVector(0, 1, 0),
       m_ViewMatrix(1),
-      m_FOV{defaultFOV},
       m_ZNear{defaultZNear},
       m_ZFar{defaultZFar} {
+  updateViewMatrix(false);
 }
 
 Camera::Camera(float x, float y, float z)
@@ -25,10 +25,9 @@ Camera::Camera(float x, float y, float z)
       m_Center(0),
       m_UpVector(0, 1, 0),
       m_ViewMatrix(1),
-      m_FOV{defaultFOV},
       m_ZNear{defaultZNear},
       m_ZFar{defaultZFar} {
-  updateViewMatrix();
+  updateViewMatrix(false);
 }
 
 Camera::Camera(math::vec3 const& position)
@@ -36,95 +35,83 @@ Camera::Camera(math::vec3 const& position)
       m_Center(0),
       m_UpVector(0, 1, 0),
       m_ViewMatrix(1),
-      m_FOV{defaultFOV},
       m_ZNear{defaultZNear},
       m_ZFar{defaultZFar} {
-  updateViewMatrix();
+  updateViewMatrix(false);
 }
 
-Camera& Camera::lookAt(float x, float y, float z) {
+void Camera::lookAt(float x, float y, float z) {
   m_Center(x, y, z);
-  updateViewMatrix();
-  return *this;
+  updateViewMatrix(true);
 }
 
-Camera& Camera::lookAt(math::vec3 const& center) {
+void Camera::lookAt(math::vec3 const& center) {
   m_Center = center;
-  updateViewMatrix();
-  return *this;
+  updateViewMatrix(true);
 }
 
-Camera& Camera::translate(float x, float y, float z) {
+void Camera::translate(float x, float y, float z) {
   m_Position.x() += x;
   m_Position.y() += y;
   m_Position.z() += z;
-  updateViewMatrix();
-  return *this;
+  updateViewMatrix(true);
 }
 
-Camera& Camera::translate(math::vec3 const& amount) {
+void Camera::translate(math::vec3 const& amount) {
   return translate(amount.x(), amount.y(), amount.z());
 }
 
-Camera& Camera::translateX(float x) {
+void Camera::translateX(float x) {
   return translate(x, 0, 0);
 }
 
-Camera& Camera::translateY(float y) {
+void Camera::translateY(float y) {
   return translate(0, y, 0);
 }
 
-Camera& Camera::translateZ(float z) {
+void Camera::translateZ(float z) {
   return translate(0, 0, z);
 }
 
-Camera& Camera::setPosition(float x, float y, float z) {
+void Camera::setPosition(float x, float y, float z) {
   m_Position(x, y, z);
-  updateViewMatrix();
-  return *this;
+  updateViewMatrix(true);
 }
 
-Camera& Camera::setPosition(math::vec3 const& position) {
+void Camera::setPosition(math::vec3 const& position) {
   return setPosition(position.x(), position.y(), position.z());
 }
 
-Camera& Camera::setX(float x) {
+void Camera::setX(float x) {
   return setPosition(x, m_Position.y(), m_Position.z());
 }
 
-Camera& Camera::setY(float y) {
+void Camera::setY(float y) {
   return setPosition(m_Position.x(), y, m_Position.z());
 }
 
-Camera& Camera::setZ(float z) {
+void Camera::setZ(float z) {
   return setPosition(m_Position.x(), m_Position.y(), z);
 }
 
-Camera& Camera::setUpVector(math::vec3 const& upVector) {
+void Camera::setUpVector(math::vec3 const& upVector) {
   m_UpVector = upVector;
-  updateViewMatrix();
-  return *this;
+  updateViewMatrix(true);
 }
 
-Camera& Camera::setFOV(float fov) {
-  assert(fov > 0);
-  m_FOV = fov;
-  return *this;
-}
-
-Camera& Camera::setZNear(float zNear) {
+void Camera::setZNear(float zNear) {
   assert(zNear > 0 && zNear < m_ZFar);
   m_ZNear = zNear;
-  return *this;
+  updateProjectionMatrix(true);
 }
 
-Camera& Camera::setZFar(float zFar) {
+void Camera::setZFar(float zFar) {
   assert(zFar > m_ZNear);
   m_ZFar = zFar;
-  return *this;
+  updateProjectionMatrix(true);
 }
 
-math::vec3 const& Camera::getPosition() const {
+math::vec3 Camera::getPosition() const {
   return m_Position;
 }
 
@@ -140,10 +127,6 @@ float Camera::getZ() const {
   return m_Position.z();
 }
 
-float Camera::getFOV() const {
-  return m_FOV;
-}
-
 float Camera::getZNear() const {
   return m_ZNear;
 }
@@ -152,11 +135,23 @@ float Camera::getZFar() const {
   return m_ZFar;
 }
 
-math::mat4 const& Camera::viewMatrix() const {
+math::mat4 Camera::getCameraMatrix() const {
+  return m_CameraMatrix;
+}
+
+math::mat4 Camera::getViewMatrix() const {
   return m_ViewMatrix;
 }
 
-void Camera::updateViewMatrix() {
+math::mat4 Camera::getProjectionMatrix() const {
+  return m_ProjectionMatrix;
+}
+
+void Camera::updateCameraMatrix() {
+  m_CameraMatrix = m_ProjectionMatrix * m_ViewMatrix;
+}
+
+void Camera::updateViewMatrix(bool updateCameraMatrix) {
   math::vec3 const f((m_Center - m_Position).normalized());
   math::vec3 const s(f.cross(m_UpVector).normalized());
   math::vec3 const u(s.cross(f));
@@ -176,6 +171,113 @@ void Camera::updateViewMatrix() {
   result[3][2] = f.dot(m_Position);
 
   m_ViewMatrix = result;
+
+  if (updateCameraMatrix) {
+    this->updateCameraMatrix();
+  }
+}
+
+OrthograficCamera::OrthograficCamera() : m_Left{-1}, m_Right{1}, m_Bottom{-1}, m_Top{1} {
+  updateProjectionMatrix(true);
+}
+
+OrthograficCamera::OrthograficCamera(float x, float y, float z, float left, float right,
+                                     float bottom, float top)
+    : Camera::Camera(x, y, z), m_Left{-1}, m_Right{1}, m_Bottom{-1}, m_Top{1} {
+  updateProjectionMatrix(true);
+}
+
+OrthograficCamera::OrthograficCamera(math::vec3 const& position, float left, float right,
+                                     float bottom, float top)
+    : Camera::Camera(position), m_Left{-1}, m_Right{1}, m_Bottom{-1}, m_Top{1} {
+  updateProjectionMatrix(true);
+}
+
+void OrthograficCamera::setLeft(float left) {
+  m_Left = left;
+  updateProjectionMatrix(true);
+}
+
+void OrthograficCamera::setRight(float right) {
+  m_Right = right;
+  updateProjectionMatrix(true);
+}
+
+void OrthograficCamera::setBottom(float bottom) {
+  m_Bottom = bottom;
+  updateProjectionMatrix(true);
+}
+
+void OrthograficCamera::setTop(float top) {
+  m_Top = top;
+  updateProjectionMatrix(true);
+}
+
+float OrthograficCamera::getLeft() const {
+  return m_Left;
+}
+
+float OrthograficCamera::getRight() const {
+  return m_Right;
+}
+
+float OrthograficCamera::getBottom() const {
+  return m_Bottom;
+}
+
+float OrthograficCamera::getTop() const {
+  return m_Top;
+}
+
+void OrthograficCamera::updateProjectionMatrix(bool updateCameraMatrix) {
+  m_ProjectionMatrix = math::orthografic<float>(m_Left, m_Right, m_Bottom, m_Top, m_ZNear, m_ZFar);
+  if (updateCameraMatrix) {
+    this->updateCameraMatrix();
+  }
+}
+
+static const float defaultFOV = math::radians(45);
+static const float defaultAspectRatio = 1;
+
+PerspectiveCamera::PerspectiveCamera() : m_FOV{defaultFOV}, m_AspectRatio{defaultAspectRatio} {
+  updateProjectionMatrix(true);
+}
+
+PerspectiveCamera::PerspectiveCamera(float x, float y, float z, float fov, float aspectRatio)
+    : Camera::Camera(x, y, z), m_FOV{defaultFOV}, m_AspectRatio{defaultAspectRatio} {
+  updateProjectionMatrix(true);
+}
+
+PerspectiveCamera::PerspectiveCamera(math::vec3 const& position, float fov, float aspectRatio)
+    : Camera::Camera(position), m_FOV{defaultFOV}, m_AspectRatio{defaultAspectRatio} {
+  updateProjectionMatrix(true);
+}
+
+void PerspectiveCamera::setFOV(float fov) {
+  assert(fov > 0);
+  m_FOV = fov;
+  updateProjectionMatrix(true);
+}
+
+void PerspectiveCamera::setAspectRatio(float aspectRatio) {
+  assert(aspectRatio > 0);
+  m_AspectRatio = aspectRatio;
+  updateProjectionMatrix(true);
+}
+
+float PerspectiveCamera::getFOV() const {
+  return m_FOV;
+}
+
+float PerspectiveCamera::getAspectRatio() const {
+  return m_AspectRatio;
+}
+
+void PerspectiveCamera::updateProjectionMatrix(bool updateCameraMatrix) {
+  m_ProjectionMatrix = math::perspective<float>(m_FOV, m_AspectRatio, m_ZNear, m_ZFar);
+  if (updateCameraMatrix) {
+    this->updateCameraMatrix();
+  }
 }
 
 }  // namespace engine
--- src/cpp/engine/camera.hpp
+++ src/cpp/engine/camera.hpp
@@ -7,46 +7,85 @@ namespace orbitals {
 namespace engine {
 
 class Camera {
- private:
+ protected:
   math::vec3 m_Position;
   math::vec3 m_Center;
   math::vec3 m_UpVector;
-  math::mat4 m_ViewMatrix;
-  float m_FOV;
-  float m_ZNear;
-  float m_ZFar;
+  math::mat4 m_CameraMatrix, m_ViewMatrix, m_ProjectionMatrix;
+  float m_ZNear, m_ZFar;
 
  public:
   Camera();
   Camera(float x, float y, float z);
   Camera(math::vec3 const& position);
-  Camera& lookAt(float x, float y, float z);
-  Camera& lookAt(math::vec3 const& center);
-  Camera& translate(float x, float y, float z);
-  Camera& translate(math::vec3 const& amount);
-  Camera& translateX(float x);
-  Camera& translateY(float y);
-  Camera& translateZ(float z);
-  Camera& setPosition(float x, float y, float z);
-  Camera& setPosition(math::vec3 const& position);
-  Camera& setX(float x);
-  Camera& setY(float y);
-  Camera& setZ(float z);
-  Camera& setUpVector(math::vec3 const& upVector);
-  Camera& setFOV(float fov);
-  Camera& setZNear(float zNear);
-  Camera& setZFar(float zFar);
-  math::vec3 const& getPosition() const;
+  void lookAt(float x, float y, float z);
+  void lookAt(math::vec3 const& center);
+  void translate(float x, float y, float z);
+  void translate(math::vec3 const& amount);
+  void translateX(float x);
+  void translateY(float y);
+  void translateZ(float z);
+  void setPosition(float x, float y, float z);
+  void setPosition(math::vec3 const& position);
+  void setX(float x);
+  void setY(float y);
+  void setZ(float z);
+  void setUpVector(math::vec3 const& upVector);
+  void setZNear(float zNear);
+  void setZFar(float zFar);
+  math::vec3 getPosition() const;
   float getX() const;
   float getY() const;
   float getZ() const;
-  float getFOV() const;
   float getZNear() const;
   float getZFar() const;
-  math::mat4 const& viewMatrix() const;
+  math::mat4 getCameraMatrix() const;
+  math::mat4 getViewMatrix() const;
+  math::mat4 getProjectionMatrix() const;
 
- private:
-  void updateViewMatrix();
+ protected:
+  void updateCameraMatrix();
+  void updateViewMatrix(bool updateCameraMatrix = true);
+  virtual void updateProjectionMatrix(bool updateCameraMatrix = true) = 0;
+};
+
+class OrthograficCamera : public Camera {
+ protected:
+  float m_Left, m_Right, m_Bottom, m_Top;
+
+ public:
+  OrthograficCamera();
+  OrthograficCamera(float x, float y, float z, float left, float right, float bottom, float top);
+  OrthograficCamera(math::vec3 const& position, float left, float right, float bottom, float top);
+  void setLeft(float left);
+  void setRight(float right);
+  void setBottom(float bottom);
+  void setTop(float top);
+  float getLeft() const;
+  float getRight() const;
+  float getBottom() const;
+  float getTop() const;
+
+ protected:
+  void updateProjectionMatrix(bool updateCameraMatrix = true) override;
+};
+
+class PerspectiveCamera : public Camera {
+ protected:
+  float m_FOV;
+  float m_AspectRatio;
+
+ public:
+  PerspectiveCamera();
+  PerspectiveCamera(float x, float y, float z, float fov, float aspectRatio);
+  PerspectiveCamera(math::vec3 const& position, float fov, float aspectRatio);
+  void setFOV(float fov);
+  void setAspectRatio(float aspectRatio);
+  float getFOV() const;
+  float getAspectRatio() const;
+
+ protected:
+  void updateProjectionMatrix(bool updateCameraMatrix = true) override;
 };
 
 }  // namespace engine
