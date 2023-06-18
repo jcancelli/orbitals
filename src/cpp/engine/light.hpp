0c93aec99c3ed3e4d0bc81d955d96ae97e31dfff
jcancelli <jonathan.cancelli.46@gmail.com>
2023-06-18 11:15:27 +0000

    Reworked DirectionalLight

--- src/cpp/engine/light.hpp
+++ src/cpp/engine/light.hpp
@@ -7,26 +7,21 @@ namespace orbitals {
 
 namespace engine {
 
-class Light {
+class DirectionalLight {
  private:
   math::vec3 m_Color;
-
- public:
-  Light();
-  Light(math::vec3 color);
-  math::vec3 getColor() const;
-  void setColor(math::vec3 const& color);
-};
-
-class DirectionalLight : public Light {
- private:
   math::vec3 m_InvertedDirection;
+  OrthograficCamera m_Camera;
 
  public:
   DirectionalLight();
   DirectionalLight(math::vec3 invertedDirection, math::vec3 color = math::vec3(1, 1, 1));
+  math::vec3 getColor() const;
+  void setColor(math::vec3 const& color);
   math::vec3 getInvertedDirection() const;
   void setInvertedDirection(math::vec3 const& invertedDirection);
+  OrthograficCamera const& getCamera() const;
+  void setEffectedVolume(float left, float right, float bottom, float top);
 };
 
 }  // namespace engine
