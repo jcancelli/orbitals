1a9edf5293c248d4425e79a51541e13f27e5449c
jcancelli <jonathan.cancelli.46@gmail.com>
2023-06-25 13:30:52 +0000

    Fix winding order for texture test polygon

--- src/cpp/engine/texture_test.cpp
+++ src/cpp/engine/texture_test.cpp
@@ -9,7 +9,7 @@ namespace engine {
 Texture2DTest::Texture2DTest()
     : m_VBO({SimpleVertex{-1, 1, 0, 1}, SimpleVertex{-1, -1, 0, 0}, SimpleVertex{1, 1, 1, 1},
              SimpleVertex{1, -1, 1, 0}}),
-      m_IBO({0, 2, 1, 2, 1, 3}),
+      m_IBO({0, 1, 2, 2, 1, 3}),
       m_VAO(&m_VBO),
       m_Shader(util::read_string("shaders/debug_texture.vert"),
                util::read_string("shaders/debug_texture.frag")),
@@ -21,9 +21,9 @@ void Texture2DTest::draw(Texture2D& texture) {
   m_IBO.bind();
   m_Shader.bind();
   texture.setActive(GL_TEXTURE0);
-  texture.bind();
+  m_Shader.setUniform1i("uTexture", 0);
   glCall(glViewport(0, 0, m_Viewport.getWidth(), m_Viewport.getHeight()));
-  glCall(glClearColor(1, 1, 1, 1));
+  glCall(glClearColor(1, 0, 0, 1));
   glCall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
   glCall(glDrawElements(GL_TRIANGLES, m_IBO.count(), GL_UNSIGNED_INT, 0));
 }
