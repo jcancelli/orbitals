3420941ed5bf37077c5c84d9a169ea0413707666
jcancelli <jonathan.cancelli.46@gmail.com>
2023-07-14 18:50:23 +0000

    Add ONE_DEGREE_IN_RAD and ONE_RADIAN_IN_DEG constants

--- src/cpp/math/trigonometry.cpp
+++ src/cpp/math/trigonometry.cpp
@@ -5,11 +5,11 @@ namespace orbitals {
 namespace math {
 
 double radians(double deg) {
-  return deg * 0.01745329251994329576923690768489;
+  return deg * ONE_DEGREE_IN_RAD;
 }
 
 double degrees(double rad) {
-  return rad * 57.295779513082320876798154814105;
+  return rad * ONE_RADIAN_IN_DEG;
 }
 
 }  // namespace math
--- src/cpp/math/trigonometry.hpp
+++ src/cpp/math/trigonometry.hpp
@@ -8,6 +8,9 @@ namespace orbitals {
 
 namespace math {
 
+double const ONE_DEGREE_IN_RAD = 0.01745329251994329576923690768489;
+double const ONE_RADIAN_IN_DEG = 57.295779513082320876798154814105;
+
 double radians(double deg);
 double degrees(double rad);
 
