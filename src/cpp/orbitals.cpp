03f481e5c87291c5c452d48f1102fb39b92dac1b
jcancelli <jonathan.cancelli.46@gmail.com>
2023-07-15 15:41:44 +0000

    Utilize OrbitalWidget and DebugWidget in Orbitals

--- src/cpp/orbitals.cpp
+++ src/cpp/orbitals.cpp
@@ -4,22 +4,23 @@
 
 #include "engine/application.hpp"
 #include "sample_scene.hpp"
+#include "widget/debug.hpp"
+#include "widget/orbital.hpp"
 
 namespace orbitals {
 
 class Orbitals : public engine::Application {
+  widget::OrbitalWidget orbitalWidget;
+  widget::DebugWidget debugWidget;
+
  public:
   Orbitals() {
     setScene(std::shared_ptr<SampleScene>(new SampleScene));
   }
 
   void onUpdate() override {
-    ImGui::SetNextWindowPos(ImVec2(0, 0));
-    ImGui::SetNextWindowBgAlpha(0.5f);
-    ImGui::Begin("#", (bool*)nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize);
-    ImGui::Text("FPS: %f", ImGui::GetIO().Framerate);
-    ImGui::Text("Delta: %f", getClock()->delta());
-    ImGui::End();
+    debugWidget.draw();
+    orbitalWidget.draw();
   }
 };
 
